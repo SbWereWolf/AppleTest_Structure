605f73a32af53e0be6de9d87207ae27064d65ab7
FORGE-JITA\ktokt <ulfnew@gmail.com>
2016-10-29 23:52:36 +0500

    + basis architecture + CRUD for Content Entity

---
+++ BusinessLogic/Content.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLogic
+{
+    public class Content
+    {
+        private DataAccess.Content Handler { get; }
+
+        public Record ContentRecord;
+        public Content()
+        {
+            Handler = new DataAccess.Content();
+            ContentRecord = new Record();
+        }
+
+        public class Record 
+        {
+            public long? Id { get; set; }
+            public long? HierachyId { get; set; }
+            public string Name { get; set; }
+            public float? ContentValue { get; set; }
+
+            public Record()
+            {
+                Reset();
+            }
+
+            public Record(DataAccess.Content.ContentRecord saveResult)
+            {
+                Reset();
+                if (saveResult != null)
+                {
+                    Id = saveResult.Id;
+                    ContentValue = saveResult.ContentValue;
+                    HierachyId = saveResult.HierachyId;
+                    Name = saveResult.Name;
+                }
+
+            }
+
+            private void Reset()
+            {
+                Id = null;
+                HierachyId = null;
+                Name = null;
+                ContentValue = null;
+            }
+
+        }
+
+        public List<Record> Get(long? contentId = null , long? hierachyId = null, string name = null, float? content = null)
+        {
+            List<Record> result = null;
+            var handler = Handler;
+            if (handler?.Pattern != null)
+            {
+                handler.Pattern.ContentValue = content;
+                handler.Pattern.HierachyId = hierachyId;
+                handler.Pattern.Id = contentId;
+                handler.Pattern.Name = name;
+            }
+            List<DataAccess.Content.ContentRecord> records = null;
+            if ( handler!= null )
+            {
+                records = handler.Get();
+            }
+            if (records!= null )
+            {
+                foreach (var record in records)
+                {
+                    if (record!= null)
+                    {
+                        var resultRecord = new Record
+                        {
+                            ContentValue = record.ContentValue,
+                            HierachyId = record.HierachyId,
+                            Id = record.Id,
+                            Name = record.Name
+                        };
+                        if (result == null )
+                        {
+                            result = new List<Record>();
+                        }
+                        result.Add(resultRecord);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public Record Set()
+        {
+            Record result = null ;
+            DataAccess.Content.ContentRecord saveResult = null ;
+            var handler = GetHandler();
+            if (handler != null)
+            {
+                saveResult = handler.Save();
+            }
+            if (saveResult != null )
+            {
+                result = new Record(saveResult);
+            }
+
+            return result;
+        }
+
+        private DataAccess.Content GetHandler()
+        {
+            DataAccess.Content handler = null;
+            var record = ContentRecord;
+            if (record != null)
+            {
+                handler = new DataAccess.Content
+                {
+                    Record =
+                    {
+                        HierachyId = record.HierachyId,
+                        ContentValue = record.ContentValue,
+                        Id = record.Id,
+                        Name = record.Name
+                    }
+                };
+                
+            }
+            return handler;
+        }
+
+        public bool Delete()
+        {
+            var  result = false;
+            var handler = GetHandler();
+            if (handler != null)
+            {
+                result = handler.Delete();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MSSqlDataSource/ContentHandler.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+
+namespace MSSqlDataSource
+{
+    public class NullableContent
+    {
+        public long? Id { get; set; }
+        public long? HierachyId { get; set; }
+        public string Name { get; set; }
+        public float? ContentValue { get; set; }
+
+        public NullableContent()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            Id = null;
+            HierachyId = null;
+            Name = null;
+            ContentValue = null;
+        }
+    }
+
+    public class ContentHandler : DataSource
+    {
+        private Table<Content> DataSource { get; }
+        public NullableContent SearchPattern { get; set; }
+        public NullableContent Fields { private get; set; }
+
+        public ContentHandler()
+        {
+            var appleStructure = DbContext;
+            if (appleStructure != null)
+            {
+                DataSource = appleStructure.Contents;
+            }
+            SearchPattern = new NullableContent();
+            Fields = new NullableContent();
+        }
+
+        public List<NullableContent> Get()
+        {
+            IQueryable<Content> records = DataSource;
+            var pattern = SearchPattern;
+
+            if (records != null
+                && pattern != null )
+            {
+                var hierachyId = pattern.HierachyId;
+                if (hierachyId.HasValue)
+                {
+                    records = records.Where(x=>x.Hierarchy == hierachyId.Value);
+                }
+                var id = pattern.Id;
+                if (id.HasValue)
+                {
+                    records = records.Where(x => x.Id == id.Value);
+                }
+                var name = pattern.Name;
+                if (name != null )
+                {
+                    records = records.Where(x => x.Name.Contains(name));
+                }
+                var content = pattern.ContentValue;
+                if (content.HasValue)
+                {
+                    records = records.Where(x => x.ContentValue == content.Value);
+                }
+            }
+
+            List<NullableContent> result = null;
+            if (records != null)
+            {
+                foreach (var record in records)
+                {
+                    var resultRecord = CovertToNullableContent(record);
+                    if (resultRecord != null)
+                    {
+                        if (result == null)
+                        {
+                            result = new List<NullableContent>();
+                        }
+                        result.Add(resultRecord);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static NullableContent CovertToNullableContent(Content record)
+        {
+            NullableContent resultRecord = null;
+            if (record != null)
+            {
+                resultRecord = new NullableContent();
+                if (record.ContentValue.HasValue)
+                {
+                    resultRecord.ContentValue = record.ContentValue.Value;
+                }
+                resultRecord.HierachyId = record.Hierarchy;
+                resultRecord.Id = record.Id;
+                resultRecord.Name = record.Name;
+            }
+            return resultRecord;
+        }
+
+        public NullableContent Set()
+        {
+            var fileds = Fields;
+
+            Content record = null;
+            if (fileds?.Id != null)
+            {
+                record = DataSource?.First(x => x.Id == fileds.Id.Value);
+            }
+
+            var perform = false;
+            if (record != null)
+            {
+                if (fileds.HierachyId.HasValue)
+                {
+                    record.Hierarchy = fileds.HierachyId.Value;
+                }
+                record.ContentValue = fileds.ContentValue;
+                record.Name = fileds.Name;
+
+                perform = true;
+            }
+
+            var isSuccess = false;
+            if (perform)
+            {
+                DataSource.Context?.SubmitChanges();
+                isSuccess = true;
+            }
+
+            NullableContent result = null;
+            if (isSuccess)
+            {
+                result = new NullableContent
+                {
+                    Id = record.Id,
+                    ContentValue = record.ContentValue,
+                    HierachyId = record.Hierarchy,
+                    Name = record.Name
+                };
+            }
+
+            return result;
+        }
+
+        public bool Delete()
+        {
+            var result = false;
+            var fields = Fields;
+
+            Content record = null;
+            DataContext context = null;
+            if (fields?.Id != null
+                && DataSource!= null)
+            {
+                record = DataSource.First(x => x.Id == fields.Id.Value);
+                context = DataSource.Context;
+            }
+            if (record != null && context != null )
+            {
+                DataSource.DeleteOnSubmit(record);
+                context.SubmitChanges();
+                result = true;
+            }
+
+            return result;
+        }
+
+        public NullableContent Add()
+        {
+            Content record = null;
+
+            var dataSource = DataSource;
+            var fields = Fields;
+            if ( dataSource != null && fields != null)
+            {
+                record = new Content();
+            }
+
+            if (record != null )
+            {
+                record.ContentValue = fields.ContentValue;
+                record.Name = fields.Name;
+            }
+            if ( record != null && fields.Id != null)
+            {
+                record.Id = fields.Id.Value;
+            }
+
+            if (fields?.HierachyId != null && record != null)
+            {
+                record.Hierarchy = fields.HierachyId.Value;
+            }
+
+            if (record != null )
+            {
+                dataSource.InsertOnSubmit(record);
+                dataSource.Context?.SubmitChanges();
+            }
+            var result = CovertToNullableContent(record);
+
+            return result;
+        }
+    }
+}
